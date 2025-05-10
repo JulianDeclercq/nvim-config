8a44ac280d9e9dd69db551b9b43c18bdc95e135d
Julian Declercq <julian.declercq1@gmail.com>
2025-05-10 17:08:01 +0200

    add vim golf

--- init.lua
+++ init.lua
@@ -964,7 +964,9 @@ require('lazy').setup({
       { 'cs', mode = 'n' },
     },
   },
-
+  {
+    'vuciv/golf',
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
