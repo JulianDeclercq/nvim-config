eebebde9c604ce09348dc75f8c2ae07eaf25351a
Julian Declercq <julian.declercq1@gmail.com>
2025-06-09 15:08:19 +0200

    small cleanup

--- init.lua
+++ init.lua
@@ -136,8 +136,6 @@ vim.keymap.set('n', '<leader>w', '<C-w>', { noremap = true, silent = true, desc 
 vim.keymap.set('v', '<leader>w', '<C-w>', { noremap = true, silent = true, desc = 'Window command prefix (visual)' })
 -- open oldfiles in Telescope
 vim.keymap.set('n', '<leader>of', "<cmd>lua require('telescope.builtin').oldfiles()<CR>", { noremap = true, silent = true })
--- obsidian keybinds
---vim.
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
