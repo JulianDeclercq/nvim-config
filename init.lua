d626c2b4eff6711b80757b8d19a71b6406da90ee
Julian Declercq <julian.declercq1@gmail.com>
2025-06-09 10:53:42 +0200

    updated line number colour

--- init.lua
+++ init.lua
@@ -828,6 +828,13 @@ require('lazy').setup({
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
+        on_highlights = function(hl, _)
+          -- all following settings are for when 'relativenumber' is on (always in my case)
+          local color = '#5C8699'
+          hl.LineNrAbove = { fg = color }
+          hl.CursorLineNr = { fg = color }
+          hl.LineNrBelow = { fg = color }
+        end,
       }
 
       -- Load the colorscheme here.
