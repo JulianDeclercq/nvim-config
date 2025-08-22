320bf8fd11979da0634ab6661993acbde42726be
Julian Declercq <julian.declercq1@gmail.com>
2025-08-22 16:08:21 +0200

    disable footer on obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,5 +1,4 @@
 -- Make sure to install RipGrep
-
 -- Platform specific paths
 local obsidian_path
 if vim.fn.has 'macunix' == 1 then
@@ -10,33 +9,46 @@ else
   obsidian_path = vim.fn.expand '~' .. '/Repositories/obsidian'
 end
 
--- File name override, default for this plugin is a Zettelkasten id, I don't like it
-local noteIdFunction = function(title)
-  local name = title:gsub('[\\/:*?"<>|]', '') -- trim illegal characters
-  name = name:match '^%s*(.-)%s*$' -- trim leading and trailing whitespace
-
-  local candidate = name
-  local fullpath = string.format('%s/%s.md', obsidian_path, candidate)
-
-  -- if the file already exists, bump a counter
-  if vim.loop.fs_stat(fullpath) then
-    local i = 1
-    repeat
-      candidate = string.format('%s(%d)', name, i)
-      fullpath = string.format('%s/%s.md', obsidian_path, candidate)
-      i = i + 1
-    until not vim.loop.fs_stat(fullpath)
-  end
-
-  return candidate
-end
-
 return {
   'obsidian-nvim/obsidian.nvim',
   version = '*', -- use latest release
   lazy = true,
   event = 'VimEnter', -- load on startup (no file)…, looks like this also works if you do any file so it's kind of pointless for me at the moment, need to figure out why this is or if there's another solution
   ft = 'markdown', -- …or on any markdown buffer
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'hrsh7th/nvim-cmp', -- autocomplete for note references
+    'nvim-telescope/telescope.nvim', -- for snippet search
+  },
+  opts = {
+    workspaces = {
+      {
+        name = 'vault',
+        path = obsidian_path,
+      },
+    },
+    -- note_id_func = noteIdFunction,
+    follow_url_func = function(url)
+      -- Open the URL in the default web browser.
+      vim.ui.open(url)
+    end,
+    -- Where to put new notes. Valid options are
+    --  * "current_dir" - put new notes in same directory as the current buffer.
+    --  * "notes_subdir" - put new notes in the default notes subdirectory.
+    new_notes_location = 'notes_subdir',
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+      file_popup = true,
+    },
+    ui = {
+      enable = false,
+    },
+    legacy_commands = false,
+    footer = {
+      enabled = false,
+    },
+  },
   keys = {
     {
       '<leader>on',
@@ -79,38 +91,5 @@ return {
       end,
       desc = '[O]bsidian: [S]nippets',
     },
-  },
-
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'hrsh7th/nvim-cmp', -- autocomplete for note references
-    'nvim-telescope/telescope.nvim', -- for snippet search
-  },
-
-  opts = {
-    workspaces = {
-      {
-        name = 'vault',
-        path = obsidian_path,
-      },
-    },
-    -- note_id_func = noteIdFunction,
-    follow_url_func = function(url)
-      -- Open the URL in the default web browser.
-      vim.ui.open(url)
-    end,
-    -- Where to put new notes. Valid options are
-    --  * "current_dir" - put new notes in same directory as the current buffer.
-    --  * "notes_subdir" - put new notes in the default notes subdirectory.
-    new_notes_location = 'notes_subdir',
-    completion = {
-      nvim_cmp = true,
-      min_chars = 2,
-      file_popup = true,
-    },
-    ui = {
-      enable = false,
-    },
-    legacy_commands = false,
   },
 }
