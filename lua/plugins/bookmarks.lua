e68e2f6c90c65025ad51339ff5ae0fbe327b6841
Julian Declercq <julian.declercq1@gmail.com>
2025-08-29 16:40:40 +0200

    Fix bookmarks version

--- lua/plugins/bookmarks.lua
+++ lua/plugins/bookmarks.lua
@@ -1,14 +1,15 @@
 return {
   'tomasky/bookmarks.nvim',
   dependencies = {}, -- telescope loads the extension, don't need to add as a dependency
-  version = '*',
+  -- version = '*',
+  branch = 'main',
   lazy = true,
   config = function()
     local bm = require 'bookmarks'
     bm.setup()
 
     vim.keymap.set('n', '<leader>bmt', bm.bookmark_toggle, { desc = '[B]ook[m]ark [T]oggle' })
-
+    vim.keymap.set('n', '<leader>bmda', bm.bookmark_clear_all, { desc = '[B]ook[m]ark [D]elete [A]ll' })
     -- Reference from documentation if I want to add some keymaps in the future
     -- vim.keymap.set("n","mi",bm.bookmark_ann) -- add or edit mark annotation at current line
     -- vim.keymap.set("n","mc",bm.bookmark_clean) -- clean all marks in local buffer
