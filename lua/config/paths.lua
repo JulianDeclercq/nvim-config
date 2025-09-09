9b4dcebd584ea689bcca77d8e00f2840dded33ed
Julian Declercq <julian.declercq1@gmail.com>
2025-09-09 22:42:47 +0200

    Created config/paths.lua module and moved obsidian path there

---
+++ lua/config/paths.lua
@@ -0,0 +1,12 @@
+local module = {}
+
+-- Obsidian platform specific paths
+if vim.fn.has 'macunix' == 1 then
+  module.obsidian = '/Users/Julian/Repositories/obsidian'
+elseif vim.fn.has 'win32' == 1 then
+  module.obsidian = 'C:\\Users\\Julian\\Documents\\The Cache'
+else
+  module.obsidian = vim.fn.expand '~' .. '/Repositories/obsidian'
+end
+
+return module
