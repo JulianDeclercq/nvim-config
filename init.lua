d035737eb97e82b458fc93a3879e7922cacc98fa
Julian Declercq <julian.declercq1@gmail.com>
2025-09-09 22:36:45 +0200

    Added buffer-picker.lua

--- init.lua
+++ init.lua
@@ -971,4 +971,4 @@ vim.api.nvim_create_autocmd('User', {
 
 vim.keymap.set('n', '<leader>ft', require('tekken_format').format_tekken_move, { desc = '[F]ormat [T]ekken Move' })
 vim.keymap.set('n', '<leader>ftk', require('tekken_picker').pick, { desc = '[F]ind [T]e[K]ken' })
-vim.keymap.set('n', '<leader>j', require('tekken_picker').buffer_pick, { desc = 'buf picker test' })
+vim.keymap.set('n', '<leader>fb', require('buffer-picker').pick, { desc = '[F]ind [B]uffers' })
---
+++ lua/buffer-picker.lua
@@ -0,0 +1,110 @@
+local module = {}
+
+local function get_first_alias(bufnr)
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, 20, false)
+  for _, line in ipairs(lines) do
+    if line:match '^%-%-%' then
+      return lines[line + 1]
+    end
+  end
+
+  return ''
+end
+
+function module.set_obsidian_bufname(bufnr)
+  local alias = get_first_alias(bufnr)
+  if alias == '' then
+    return
+  end
+
+  local old_name = vim.api.nvim_buf_get_name(bufnr)
+  local extension = vim.fn.fnamemodify(old_name, ':e')
+  local new_name = alias .. (extension ~= '' and ('.' .. extension) or '')
+  vim.api.nvim_buf_set_name(bufnr, new_name)
+end
+
+function module.pick()
+  local pickers = require 'telescope.pickers'
+  local finders = require 'telescope.finders'
+  local config = require('telescope.config').values
+  local actions = require 'telescope.actions'
+  local action_state = require 'telescope.actions.state'
+  local previewers = require 'telescope.previewers'
+  local putils = require 'telescope.previewers.utils'
+  local entry_display = require 'telescope.pickers.entry_display'
+
+  local displayer = entry_display.create {
+    separator = ' ',
+    items = {
+      { width = 4 }, -- bufnr
+      { remaining = true }, -- filename
+    },
+  }
+
+  local entries = {}
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(buf) then
+      local name = vim.api.nvim_buf_get_name(buf)
+      local short = name ~= '' and vim.fn.fnamemodify(name, ':t') or ('[No Name] ' .. buf)
+      table.insert(entries, {
+        value = buf,
+        ordinal = name ~= '' and name or short,
+        display = function()
+          return displayer {
+            { tostring(buf), 'TelescopeResultsNumber' }, -- shows bufnr
+            { short },
+          }
+        end,
+      })
+    end
+  end
+
+  -- Previewer: copy the selected buffer's text into the preview buffer
+  local buffer_previewer = previewers.new_buffer_previewer {
+    title = 'Grep Preview',
+    get_buffer_by_name = function(_, entry)
+      return 'telescope-bufpreview-' .. entry.value
+    end,
+    define_preview = function(self, entry, _)
+      local bufnr = entry.value
+      if not (bufnr and vim.api.nvim_buf_is_valid(bufnr)) then
+        putils.set_preview_message(self.state.bufnr, 'Invalid buffer')
+        return
+      end
+
+      local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+      vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
+
+      -- mirror filetype for syntax highlighting
+      vim.bo[self.state.bufnr].filetype = vim.bo[bufnr].filetype
+    end,
+  }
+
+  pickers
+    .new({}, {
+      prompt_title = 'Julles test',
+      finder = finders.new_table {
+        results = entries,
+        entry_maker = function(e)
+          return e
+        end,
+      },
+      sorter = config.generic_sorter {},
+      previewer = buffer_previewer,
+      attach_mappings = function(prompt_bufnr, map)
+        local open_buf = function()
+          local selection = action_state.get_selected_entry()
+          actions.close(prompt_bufnr)
+          vim.api.nvim_set_current_buf(selection.value)
+        end
+
+        map('i', '<CR>', open_buf)
+        map('n', '<CR>', open_buf)
+
+        return true
+      end,
+    })
+    :find()
+end
+
+return module
